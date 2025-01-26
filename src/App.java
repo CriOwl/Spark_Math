4fce76ecb485a767c11a2310f6951c0ce7aef222
Dalzsfn <dalz20033@gmail.com>
2025-01-26 15:30:37 -0500

    catalog_levelDAO

--- src/App.java
+++ src/App.java
@@ -1,15 +1,29 @@
+import Data_Access.DAO.Catalog_levelDAO;
 import Data_Access.DAO.PersonDAO;
+import Data_Access.DTO.Catalog_levelDTO;
 import Data_Access.DTO.PersonDTO;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try {
-            PersonDAO pdsa= new PersonDAO();
-            for (PersonDTO asdasd : pdsa.readall()) {
-                System.out.println(asdasd.toString());
+        // try {
+        //     PersonDAO pdsa= new PersonDAO();
+        //     for (PersonDTO asdasd : pdsa.readall()) {
+        //         System.out.println(asdasd.toString());
+        //     }
+        // } catch (Exception e) {
+        // } finally {
+        // }
+
+        try{
+            Catalog_levelDAO asd= new Catalog_levelDAO ();
+            for(Catalog_levelDTO dsa : asd.readall()){
+                System.out.println(dsa.toString());
             }
-        } catch (Exception e) {
-        } finally {
+        }catch(Exception e){
+            
         }
+            
+           
     }
 }
+
---
+++ src/Data_Access/DAO/Catalog_levelDAO.java
@@ -0,0 +1,141 @@
+package Data_Access.DAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import Data_Access.Data_Helper_Sqlite;
+import Data_Access.DTO.Catalog_levelDTO;
+
+public class Catalog_levelDAO extends Data_Helper_Sqlite implements IDAO<Catalog_levelDTO> {
+
+    @Override
+    public Catalog_levelDTO readby(Integer id) throws Exception {
+        Catalog_levelDTO oS = new Catalog_levelDTO();
+        String query =" SELECT id_catalog_level  " 
+                     +" ,name                    " 
+                     +" ,state                   " 
+                     +" ,date_created            " 
+                     +" ,date_updated "
+                     +" FROM    Catalog_level   "
+                     +" WHERE   state ='1' AND id_catalog_level =   "+ id.toString() ;
+        try {
+            Connection conn = opConnection();         // conectar a DB     
+            Statement  stmt = conn.createStatement();   // CRUD : select * ...    
+            ResultSet rs   = stmt.executeQuery(query);  // ejecutar la
+            while (rs.next()) {
+                oS = new Catalog_levelDTO(rs.getInt(1)       // Id_catalog_level
+                                ,rs.getString(2)             // name             
+                                ,rs.getInt(3)                // state        
+                                ,rs.getString(4)             // date_created      
+                                ,rs.getString(5));           // date_update
+            }
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "readBy()");
+        }
+        return oS;
+    }
+
+    @Override
+    public List<Catalog_levelDTO> readall() throws Exception {
+        List <Catalog_levelDTO> lst = new ArrayList<>();
+        String query =" SELECT id_catalog_level  " 
+                     +" ,name                    " 
+                     +" ,state                   " 
+                     +" ,date_created            " 
+                     +" ,date_updated "
+                     +" FROM    Catalog_level   "
+                     +" WHERE   state='1'";
+
+        try {
+            Connection conn = opConnection();         // conectar a DB     
+            Statement  stmt = conn.createStatement();   // CRUD : select * ...    
+            ResultSet rs   = stmt.executeQuery(query);    // ejecutar la
+            while (rs.next()) {
+                Catalog_levelDTO s = new Catalog_levelDTO(rs.getInt(1)       // Id_catalog_level
+                                                ,rs.getString(2)             // name             
+                                                ,rs.getInt(3)                // state        
+                                                ,rs.getString(4)             // date_created      
+                                                ,rs.getString(5));           // date_update
+             lst.add(s);
+            }
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "readAll()");
+        }
+        return lst; 
+    }
+
+    @Override
+    public boolean created(Catalog_levelDTO entity) throws Exception {
+        String query = " INSERT INTO Catalogo (Nombre) VALUES (?)";
+        try {
+            Connection        conn  = opConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, entity.getName());
+            pstmt.executeUpdate();
+            return true;
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "create()");
+        }
+    }
+
+    @Override
+    public boolean update(Catalog_levelDTO entity) throws Exception {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now();
+        String query = " UPDATE Catalogo SET Nombre = ?, Fecha_modificacion = ? WHERE Id_catalogo = ?";
+        try {
+            Connection          conn = opConnection();
+            PreparedStatement pstmt  = conn.prepareStatement(query);
+            pstmt.setString(1, entity.getName());
+            pstmt.setString(2, dtf.format(now).toString());
+            pstmt.setInt(3, entity.getId_catalog_level());
+            pstmt.executeUpdate();
+            return true;
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "update()");
+        }
+    }
+
+    @Override
+    public boolean delete(Integer id) throws Exception {
+        String query = " UPDATE Catalogo SET Estado = ? WHERE Id_catalogo = ?";
+        try {
+            Connection          conn = opConnection();
+            PreparedStatement  pstmt = conn.prepareStatement(query);
+            pstmt.setString(1, "X");
+            pstmt.setInt(2, id);
+            pstmt.executeUpdate();
+            return true;
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "delete()");
+        }
+    }
+
+    public Integer getMaxRow()  throws Exception  {
+        String query =" SELECT COUNT(*) TotalReg FROM Catalogo "
+                     +" WHERE   Estado ='A' ";
+        try {
+            Connection conn = opConnection();         // conectar a DB     
+            Statement  stmt = conn.createStatement();   // CRUD : select * ...    
+            ResultSet rs   = stmt.executeQuery(query);  // ejecutar la
+            while (rs.next()) {
+                return rs.getInt(1);                    // TotalReg
+            }
+        } 
+        catch (SQLException e) {
+            throw e;// new PatException(e.getMessage(), getClass().getName(), "getMaxRow()");
+        }
+        return 0;
+    }
+}
