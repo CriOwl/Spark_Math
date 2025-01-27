7be8ee32157727e2e1cd31f023d69122c13c4f90
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2025-01-27 11:23:18 -0500

    BL y view

--- src/App.java
+++ src/App.java
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
 import BusinessLogic.User;
-public class App {
-    public static void main(String[] args) throws Exception {
-        User system=new User();
-        system.sistema(); 
-        /* PermissionRoleDAO consulta=new PermissionRoleDAO();
-        for (Permission_roleDTO registo : consulta.role_permission_read(1)) {
-            System.out.println(registo.getPermission_name());
-        } */
-            
-        }
-
-        
-    }
-=======
 import Data_Access.DAO.Grades_Activity_game2DAO;
 import Data_Access.DAO.InstitutionDAO;
 import Data_Access.DAO.PermissionDAO;
@@ -29,68 +14,12 @@ import Data_Access.DTO.RoleDTO;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        try {
-            PersonDAO pdsa= new PersonDAO();
-            for (PersonDTO asdasd : pdsa.readall()) {
-                System.out.println("::::::::::::::::::::::");
-                System.out.println(asdasd.toString());
-            }
-        } catch (Exception e) {
-        } finally {
-        }
-
-        try {
-            PermissionDAO pdsa= new PermissionDAO();
-            for (PermissionDTO asdasd : pdsa.readall()) {
-                System.out.println("//////////////////////");
-                System.out.println(asdasd.toString());
-            }
-        } catch (Exception e) {
-        }
-
-        try {
-            PersonDAO pdsa = new PersonDAO();
-            for (PersonDTO asdasd : pdsa.readall()) {
-                System.out.println("*********************");
-                System.out.println(asdasd.toString());
-            }
-        } catch (Exception e) {
-        }
-
-        try {
-            PermissionRoleDAO pdsa = new PermissionRoleDAO();
-            for (Permission_roleDTO asdasd : pdsa.readall()) {
-                System.out.println("______________________");
-                System.out.println(asdasd.toString());
-            }
-        } catch (Exception e) {
-        }
-
-        try {
-            RoleDAO pdsa = new RoleDAO();
-            for (RoleDTO asdasd : pdsa.readall()) {
-                System.out.println("++++++++++++++++++++");
-                System.out.println(asdasd.toString());
-            }
-        } catch (Exception e) {
-        }
-
-        try{
-            InstitutionDAO pdsa = new InstitutionDAO();
-            for(InstitutionDTO  asdasd : pdsa.readall()){
-                System.out.println("-------------------");
-                System.out.println(asdasd.toString());
-            }
-        }catch(Exception e){}finally{}
-        
-        try{
-            Grades_Activity_game2DAO pdsa = new Grades_Activity_game2DAO();
-            for(Grades_Activity_game2DTO asdasd : pdsa.readall()){
-                System.out.println("-/-/-/-/-/-/-/-/-/-/");
-                System.out.println(asdasd.toString());
-            }
-        }catch (Exception e){}finally{}
+            User system=new User();
+            system.sistema(); 
+            /* PermissionRoleDAO consulta=new PermissionRoleDAO();
+            for (Permission_roleDTO registo : consulta.role_permission_read(1)) {
+                System.out.println(registo.getPermission_name());
+            } */
     }
 
 }
->>>>>>> 0296549c0af69ea1b3dd04b65d95e26a3442a689
