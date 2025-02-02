9263f17634c40cab90100ce7e6af1fbbd6e5f649
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2025-02-01 19:38:01 -0500

    Panel de administracion

---
+++ src/UserInterface/Form/Update_panel.java
@@ -0,0 +1,13 @@
+package UserInterface.Form;
+
+import javax.swing.JPanel;
+import org.w3c.dom.Text;
+
+public class Update_panel extends JPanel {
+    Text_label ;
+    public Update_panel() {
+
+    }
+    private void setup_panel() {
+    }
+}
