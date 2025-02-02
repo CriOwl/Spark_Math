2ba5e3778689d324dd7a913176e6dfc0f32de1fd
MicMiuw <karinacardenasdiaz2005@hotmail.com>
2025-02-01 23:18:58 -0500

    Update Game1DAO.java

--- src/Data_Access/DAO/Game1DAO.java
+++ src/Data_Access/DAO/Game1DAO.java
@@ -18,29 +18,29 @@ public class Game1DAO extends Data_Helper_Sqlite implements IDAO<Game1DTO>{
     @Override
     public Game1DTO readby(Integer id) throws Exception {
         Game1DTO oS = new Game1DTO();
-        String query =" SELECT id_game1      " 
-                     +" ,question            " 
-                     +" ,answer1             " 
-                     +" ,answer2             " 
-                     +" ,correct_answer      " 
-                     +" ,state               " 
-                     +" ,date_created        " 
-                     +" ,date_updated        "
-                     +" FROM    Game1        "
-                     +" WHERE   state ='1' AND id_game1 =   "+ id.toString() ;
+        String query =" SELECT id_game1                      " 
+                     +" ,question                            " 
+                     +" ,answer1                             " 
+                     +" ,answer2                             " 
+                     +" ,correct_answer                      " 
+                     +" ,state                               " 
+                     +" ,date_created                        " 
+                     +" ,date_updated                        "
+                     +" FROM    Game1                        "
+                     +" WHERE   state ='1' AND id_game1 =    "+ id.toString() ;
         try {
             Connection conn = opConnection();             
             Statement  stmt = conn.createStatement();     
-            ResultSet rs   = stmt.executeQuery(query);  
+            ResultSet rs    = stmt.executeQuery(query);  
             while (rs.next()) {
-                oS = new Game1DTO(rs.getInt(1)                // Id_game1
-                                ,rs.getString(2)              // question 
-                                ,rs.getString(3)              // answer1
-                                ,rs.getString(4)              // answer2
-                                ,rs.getInt(5)                 // correct_answer          
-                                ,rs.getInt(6)                 // state        
-                                ,rs.getString(7)              // date_created      
-                                ,rs.getString(8));            // date_update
+                oS = new Game1DTO(rs.getInt(1)                 // Id_game1
+                                 ,rs.getString(2)              // question 
+                                 ,rs.getString(3)              // answer1
+                                 ,rs.getString(4)              // answer2
+                                 ,rs.getInt(5)                 // correct_answer          
+                                 ,rs.getInt(6)                 // state        
+                                 ,rs.getString(7)              // date_created      
+                                 ,rs.getString(8));            // date_update
             }
         } 
         catch (SQLException e) {
@@ -66,16 +66,16 @@ public class Game1DAO extends Data_Helper_Sqlite implements IDAO<Game1DTO>{
         try {
             Connection conn = opConnection();           
             Statement  stmt = conn.createStatement();     
-            ResultSet rs   = stmt.executeQuery(query);    
+            ResultSet rs    = stmt.executeQuery(query);    
             while (rs.next()) {
-                Game1DTO s = new Game1DTO(rs.getInt(1)                    // id_game1
-                                            ,rs.getString(2)              // question 
-                                            ,rs.getString(3)              // answer1
-                                            ,rs.getString(4)              // answer2
-                                            ,rs.getInt(5)                 // correct_answer          
-                                            ,rs.getInt(6)                 // state        
-                                            ,rs.getString(7)              // date_created      
-                                            ,rs.getString(8));            // date_update
+                Game1DTO s = new Game1DTO(rs.getInt(1)                 // id_game1
+                                         ,rs.getString(2)              // question 
+                                         ,rs.getString(3)              // answer1
+                                         ,rs.getString(4)              // answer2
+                                         ,rs.getInt(5)                 // correct_answer          
+                                         ,rs.getInt(6)                 // state        
+                                         ,rs.getString(7)              // date_created      
+                                         ,rs.getString(8));            // date_update
              lst.add(s);
             }
         } 
@@ -106,8 +106,8 @@ public class Game1DAO extends Data_Helper_Sqlite implements IDAO<Game1DTO>{
     @Override
     public boolean update(Game1DTO entity) throws Exception {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
-        LocalDateTime now = LocalDateTime.now();
-        String query = " UPDATE Game1 SET question = ?, answer1 = ?, answer2 = ?, correct_answer = ?, date_updated = ? WHERE id_game1 = ?";
+        LocalDateTime now     = LocalDateTime.now();
+        String query          = " UPDATE Game1 SET question = ?, answer1 = ?, answer2 = ?, correct_answer = ?, date_updated = ? WHERE id_game1 = ?";
         try {
             Connection          conn = opConnection();
             PreparedStatement pstmt  = conn.prepareStatement(query);
@@ -137,7 +137,7 @@ public class Game1DAO extends Data_Helper_Sqlite implements IDAO<Game1DTO>{
             return true;
         } 
         catch (SQLException e) {
-            throw e;// new PatException(e.getMessage(), getClass().getName(), "delete()");
+            throw e;                  // new PatException(e.getMessage(), getClass().getName(), "delete()");
         }
     }
 
@@ -145,15 +145,15 @@ public class Game1DAO extends Data_Helper_Sqlite implements IDAO<Game1DTO>{
         String query =" SELECT COUNT(*) TotalReg FROM Game1 "
                      +" WHERE   state ='A' ";
         try {
-            Connection conn = opConnection();         // conectar a DB     
-            Statement  stmt = conn.createStatement();   // CRUD : select * ...    
-            ResultSet rs   = stmt.executeQuery(query);  // ejecutar la
+            Connection conn = opConnection();            // conectar a DB     
+            Statement  stmt = conn.createStatement();    // CRUD : select * ...    
+            ResultSet rs    = stmt.executeQuery(query);  // ejecutar la
             while (rs.next()) {
-                return rs.getInt(1);                    // TotalReg
+                return rs.getInt(1);                     // TotalReg
             }
         } 
         catch (SQLException e) {
-            throw e;// new PatException(e.getMessage(), getClass().getName(), "getMaxRow()");
+            throw e;                                     // new PatException(e.getMessage(), getClass().getName(), "getMaxRow()");
         }
         return 0;
     }
